fc86611f276373c89315e582044c7b1d575665d6
Mustafa Gözcü <gozcumustafa34@gmail.com>
2023-12-19 15:22:17 +0100

    Added EJS template engine and updated related configurations

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ import express from "express"
 const app = express()
 const port = 3000
 
+//Ejs template engine
+app.set('view engine','ejs');
+
+//static files middleware
+app.use(express.static('public'));
+
+
 app.get("/",(req,res) => {
-    res.send('INDEX SAYFASI');
+    res.render('index');
+})
+app.get("/about",(req,res) => {
+    res.render('about');
+})
+app.get("/",(req,res) => {
+    res.render('index');
 })
 
 app.listen(port, ()=> {
